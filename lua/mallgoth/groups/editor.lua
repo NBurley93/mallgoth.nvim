163e26996269cba1655278f701f88f4844d76631
Nicholas Burley <nick.burley@protonmail.com>
2025-10-02 11:49:38 -0500

    feat: initial commit

---
+++ lua/mallgoth/groups/editor.lua
@@ -0,0 +1,42 @@
+M = {}
+
+function M.get(P)
+    return {
+        highlights = {
+            -- core editor
+            Normal = { fg = P.fg, bg = P.transparent_bg },
+            Comment = { fg = P.muted, italic = true },
+            CursorLine = { bg = P.line_focus },
+            Visual = { bg = P.visual_bg },
+
+            -- Syntax groups
+            Keyword = { fg = P.neon_green, bold = true },
+            String = { fg = P.cursor_glow },
+            Function = { fg = P.acid_teal, bold = true },
+            Identifier = { fg = P.pale_lilac },
+            Constant = { fg = P.rose_red },
+            Error = { fg = P.harsh_magenta, bold = true },
+
+            -- search & statusline
+            StatusLine = { fg = P.neon_green, bg = P.bg1, bold = true },
+            Search = { fg = P.search_fg, bg = P.search_bg, bold = true },
+
+            -- Float windows
+            NormalFloat = { fg = P.fg, bg = P.bg0 },
+            FloatBorder = { fg = P.electric_violet, bg = P.bg0 },
+
+            -- Diagnostics
+            DiagnosticError = { fg = P.harsh_magenta },
+            DiagnosticWarn = { fg = P.sickly_yellow },
+            DiagnosticInfo = { fg = P.acid_teal },
+            DiagnosticHint = { fg = P.sickly_yellow },
+
+            -- diffs
+            DiffAdd = { fg = P.diff_add },
+            DiffDelete = { fg = P.diff_remove },
+            DiffChange = { fg = P.diff_change },
+        },
+    }
+end
+
+return M
---
+++ lua/mallgoth/init.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.colors = require('mallgoth.colors')
+M.utils = require('mallgoth.utils')
+M.editor_groups = require('mallgoth.groups.editor')
+
+function M.setup(user_conf)
+    M.utils.process_groups(M.editor_groups.get(M.colors))
+
+    -- Load all integrations
+    local integration_modules = require('mallgoth.groups.integrations')
+    for _, mod in pairs(integration_modules) do
+        if type(mod) == 'table' then
+            M.utils.process_groups(mod)
+        end
+    end
+end
+
+return M
