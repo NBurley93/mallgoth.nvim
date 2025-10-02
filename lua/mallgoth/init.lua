ecea82c45a4615a192914358ffd156c29fa38b12
Nicholas Burley <nick.burley@protonmail.com>
2025-10-02 12:17:19 -0500

    feat: First pass at integration loading

--- lua/mallgoth/init.lua
+++ lua/mallgoth/init.lua
@@ -8,10 +8,15 @@ function M.setup(user_conf)
     M.utils.process_groups(M.editor_groups.get(M.colors))
 
     -- Load all integrations
-    local integration_modules = require('mallgoth.groups.integrations')
-    for _, mod in pairs(integration_modules) do
-        if type(mod) == 'table' then
-            M.utils.process_groups(mod)
+    local integration_modules = vim.api.nvim_get_runtime_file('mallgoth/groups/integrations/*.lua', true)
+    for _, module_path in ipairs(integration_modules) do
+        local ok, loaded = pcall(require, module_path)
+        if not ok then
+            vim.notify('Error loading Mallgoth integration: ' .. module_path .. '\n\n' .. loaded, vim.log.levels.ERROR)
+        else
+            if type(loaded) == 'table' then
+                M.utils.process_groups(loaded)
+            end
         end
     end
 end
